a5de2d2513916e8001b25912352d297bf6fdd213
Blas Santomé Ocampo <bsantome@mercadona.es>
2022-11-30 20:31:20 +0100

    feat(users): add method for create and delete users

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,6 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 import { prisma } from "../index";
+import crypto from "crypto";
 
 export const getUsers = async (_, res: Response) => {
   const users = await prisma.user.findMany({
@@ -12,4 +13,31 @@ export const getUsers = async (_, res: Response) => {
     },
   });
   res.json(users);
+};
+
+export const createUser = async (req: Request, res: Response) => {
+  const { name, username, password, surnames } = req.body;
+  const cryptoPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+  const users = await prisma.user.create({
+    data: {
+      name,
+      username,
+      surnames,
+      password: cryptoPassword,
+    },
+  });
+  res.json(users);
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  const deleted = await prisma.user.delete({
+    where: {
+      id: parseInt(req.params.id),
+    },
+  });
+
+  res.status(202).json(deleted);
 };
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { checkAdmin } from "../utils/roles";
-import { getUsers } from "../controllers/users";
+import { createUser, deleteUser, getUsers } from "../controllers/users";
 
 export const usersRouter = Router();
 
 usersRouter.get("/", checkAdmin, getUsers);
+usersRouter.post("/", checkAdmin, createUser);
+usersRouter.delete("/:id", checkAdmin, deleteUser);
